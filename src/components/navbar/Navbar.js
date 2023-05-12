0e6be56b304ac341af48ae9aac63e0720275d590
Bogdana <kuzminabogdana25@gmail.com>
2023-05-12 21:33:03 +0300

    route

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
       <nav className="nav" >
          <div className="container" >
             <div className="nav-row" >
-               <NavLink to='/' className="logo">
+               <NavLink to='/frilancer/' className="logo">
                   <strong>Freelancer</strong> portfolio
                </NavLink>
 
@@ -20,14 +20,14 @@ const Navbar = () => {
 
                <ul className="nav-list" >
                   <li className="nav-list__item" >
-                     <NavLink to='/' className={({ isActive }) => isActive ? activeLink : normalLink}>
+                     <NavLink to='/frilancer/' className={({ isActive }) => isActive ? activeLink : normalLink}>
                         Home
                      </NavLink>
                   </li >
-                  <li className="nav-list__item" ><NavLink to='/projects' className={({ isActive }) => isActive ? activeLink : normalLink}>
+                  <li className="nav-list__item" ><NavLink to='/frilancer/projects' className={({ isActive }) => isActive ? activeLink : normalLink}>
                      Projects
                   </NavLink></li >
-                  <li className="nav-list__item" ><NavLink to='/contacts' className={({ isActive }) => isActive ? activeLink : normalLink}>
+                  <li className="nav-list__item" ><NavLink to='/frilancer/contacts' className={({ isActive }) => isActive ? activeLink : normalLink}>
                      Contacts
                   </NavLink></li >
                </ul >
